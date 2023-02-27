9c1e339aa4fdafee985e477aaf2424fda9d71470
Anastasya - art <anastasya12az@gmail.com>
2023-02-27 22:00:18 +0300

    Add update task01

--- zaharova_ad/main.py
+++ zaharova_ad/main.py
@@ -1,28 +1,44 @@
 import sys
 
 
-# --------------------Чтения файла--------------------------
+# Code 1 - incorrect parameter -f
+# Code 2 - incorrect parameter -n
 
-def open_file(file_split_data):
+
+# --------------------Проверка аргументов командной строки--------------------------
+def check_parameter():
     if '-f' not in sys.argv:
-        print('Error parameter')
-        sys.exit(0)
+        sys.exit(f'Error parameter with code 1')
+    if '-n' not in sys.argv:
+        sys.exit(f'Error parameter with code 2')
+    if '-d' in sys.argv:
+        return 1
+    if '-l' in sys.argv:
+        return 2
 
+    return 0
+
+
+# --------------------Чтения файла и разделение на строки--------------------------
+
+def read_split_file(file_split_data):
     filename = sys.argv[sys.argv.index('-f') + 1]
 
-    with open(file=filename, mode='r', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         file_data = file.readlines()  # -> [str_1, str_2, str_3]
-        if '-l' in sys.argv:
-            file_split_data = bonus_param_l(file_data)
+        check_result = check_parameter()
+        if check_result == 2:
+            file_split_data = merge_line_tag(file_data)
             return file_split_data
         for line in file_data:
             file_split_data.extend(line.split())  # str_1 -> [word_1, word_2, ...]
             file_split_data.append('\n')
-        return file_split_data
+
+    return file_split_data
 
 
 # --------------------Bonus 1---------------------------
-def bonus_param_l(file_data):
+def merge_line_tag(file_data):
     file_split_data = []
 
     for line in file_data:
@@ -40,79 +56,77 @@ def bonus_param_l(file_data):
 def merge_words(file_split_data: list):
     nickname_index = None
 
-    for i in range(len(file_split_data)):
-        if not file_split_data[i]:
+    for index in range(len(file_split_data)):
+        if not file_split_data[index]:
             continue
-        if file_split_data[i][0] == '@' and file_split_data[i][-1] != ':':
-            nickname_index = i
+        if file_split_data[index][0] == '@' and file_split_data[index][-1] != ':':
+            nickname_index = index
         if nickname_index is not None:
-            file_split_data[nickname_index] += ' ' + file_split_data[i + 1]
-            if file_split_data[i + 1][-1] == ':':
+            file_split_data[nickname_index] += ' ' + file_split_data[index + 1]
+            if file_split_data[index + 1][-1] == ':':
                 nickname_index = None
-            file_split_data[i + 1] = ''
+            file_split_data[index + 1] = ''
     return file_split_data
 
 
 # -----------------------Получить подстроки,ограниченные по длине-------------------------------
 
 def split_line(file_split_data: list):
-    if '-n' not in sys.argv:
-        print('Error parameter')
-        sys.exit(0)
-
+    check_parameter()
     result = ''
     substring = ''
     i = 1
-    char_number = int(sys.argv[sys.argv.index('-n') + 1])
 
-    if not int(sys.argv[sys.argv.index('-n') + 1]):
+    try:
+        char_number = int(sys.argv[sys.argv.index('-n') + 1])
+    except TypeError:
         char_number = 200
 
-    for index in range(len(file_split_data)):
-        if len(substring) + len(file_split_data[index]) <= char_number:
-            if not file_split_data[index]:
+    for word in file_split_data:
+        if len(substring) + len(word) <= char_number:
+            if not word:
                 continue
-            if file_split_data[index][0] == '-':
+            if word[0] == '-':
                 substring += '\t'
-            substring += file_split_data[index]
-            if file_split_data[index][-1] != '\n':
+            substring += word
+            if word[-1] != '\n':
                 substring += ' '
             continue
         result += f'Substring #{i}:\n' + substring + '\n'
-        if '-d' in sys.argv:
-            save_file(substring, i)
+        print(result)
         substring = ''
-        if file_split_data[index][0] == '-':
+        if word[0] == '-':
             substring += '\t'
-        substring += file_split_data[index] + ' '
+        substring += word + ' '
+        check_result = check_parameter()
+        if check_result == 1:
+            save_file(result, i)
+        result = ''
         i += 1
 
     result += f'Substring #{i}:\n' + substring + '\n'
-    if '-d' in sys.argv:
-        save_file(substring, i)
-    print_line(result)
+    check_result = check_parameter()
+    if check_result == 1:
+        save_file(result, i)
+    print(result)
 
 
 # -------------Bonus 2---------------------
 
 def save_file(result, index_sub):
-    sub_file = open(f'substring_{index_sub}.txt', 'w')
-    sub_file.write(f'{result}\n')
-    sub_file.close()
+    with open(f'substring_{index_sub}.txt', 'w') as file:
+        file.write(f'{result}\n')
     return
 
 
-# -----------------------------Вывод строки---------------------
-def print_line(result):
-    print(result)
-
+# -----------------------------Вызов функций---------------------
 
 def main():
+    check_parameter()
     data = []
-    data = open_file(data)
-    if '-l' not in sys.argv:
-        data = merge_words(data)
-    split_line(data)
+    data = read_split_file(data)
+    merge_lines = merge_words(data)
+    split_line(merge_lines)
 
 
 if __name__ == '__main__':
