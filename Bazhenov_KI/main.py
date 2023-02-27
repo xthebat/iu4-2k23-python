2d2e1f81a5321eea0e9266414cef956655f0f6d3
kotyakostya1 <konstantin.bazhenov.01@mail.ru>
2023-02-27 10:13:49 +0300

    Fixed

--- Bazhenov_KI/main.py
+++ Bazhenov_KI/main.py
@@ -1,106 +1,101 @@
 import sys
 
-def processing_and_cropping_str(text, max_index, start_index, INDEX_INC, l_rule, d_rule, file_cnt) -> int:
+
+def processing_and_cropping_str(text, max_cnt, start_index, index_inc, l_rule):
     cnt = start_index
     end_index = -1
     text_length = len(text)
-    if cnt == text_length + 1:
-        print('Строки разделены')
-        return 0
-    if text_length - cnt < INDEX_INC:
-            if print_string(text_length, max_index, text, start_index, INDEX_INC, l_rule, d_rule, file_cnt) == 0:
-                return 0
-    while ((cnt <= max_index) and cnt != text_length):
-        if text[cnt] == '@':
-            cnt_save = cnt
-            temp = text.find(':', cnt, text_length - 1)
-            if temp < (max_index - 1) and temp != -1:
-                cnt = temp + 1
-                if (l_rule == 1):
-                    cnt += 1
-                    while text[cnt].isnumeric() == 1:
+    list_of_str = []
+    while cnt != text_length + 1:
+        if text_length - cnt < index_inc:
+            one_of_lines = text[start_index:text_length]
+            list_of_str.append(one_of_lines)
+            break
+        while (cnt <= max_cnt) and cnt != text_length:
+            if text[cnt] == '@':
+                cnt_save = cnt
+                temp = text.find(':', cnt, text_length - 1)
+                if temp < (max_cnt - 1) and temp != -1:
+                    cnt = temp + 1
+                    if l_rule == 1:
                         cnt += 1
-                        if text[cnt] == text[-1]:
-                            break
-                    if cnt >= max_index and end_index == -1:
-                        print('Невозможно разделить следующую строку ')
-                        return 0
-                    if cnt >= max_index:
-                        cnt = cnt_save + 1
-            elif temp == -1:
-                cnt += 1
-            elif end_index == -1:
-                print('Невозможно разделить следующую строку ')
-                return 0
+                        while text[cnt].isnumeric() == 1:
+                            cnt += 1
+                            if text[cnt] == text[-1]:
+                                break
+                        if cnt >= max_cnt and end_index == -1:
+                            print('Невозможно разделить следующую строку ')
+                            return
+                        if cnt >= max_cnt:
+                            cnt = cnt_save + 1
+                elif temp == -1:
+                    cnt += 1
+                elif end_index == -1:
+                    print('Невозможно разделить следующую строку ')
+                    return
+                else:
+                    one_of_lines = text[start_index:end_index]
+                    list_of_str.append(one_of_lines)
+                    start_index = end_index + 1
+                    max_cnt = start_index + index_inc
+            if text[cnt] == ' ' or text[cnt] == '\n':
+                end_index = cnt
+                if cnt - 1 < max_cnt:
+                    cnt += 1
             else:
-                if print_string(end_index, max_index, text, start_index, INDEX_INC, l_rule, d_rule, file_cnt) == 0:
-                    return 0
-        if text[cnt] == ' ' or text[cnt] == '\n':
-            end_index = cnt
-            if cnt - 1 < max_index:
                 cnt += 1
-        else:
-            cnt += 1
-    if end_index == -1:
-        print('Невозможно разделить строку')
-        return 0
-    if print_string(end_index, max_index, text, start_index, INDEX_INC, l_rule, d_rule, file_cnt) == 0:
-        return 0
+        if end_index == -1:
+            print('Невозможно разделить строку')
+            return
+        one_of_lines = text[start_index:end_index]
+        list_of_str.append(one_of_lines)
+        start_index = end_index + 1
+        max_cnt = start_index + index_inc
+    return list_of_str
 
-def print_string(end_index, max_index, text, start_index, INDEX_INC, l_rule, d_rule, file_cnt) -> int:
-    final_str = text[start_index:end_index]
-    if d_rule == 1:
-        with open (sys.argv[7] + "str" + str(file_cnt) + ".txt", "w") as new_file:
-            new_file.write("%s" % final_str)
-        file_cnt += 1
-    print(final_str)
-    start_index = end_index + 1
-    max_index = start_index + INDEX_INC
-    if processing_and_cropping_str(text, max_index, start_index, INDEX_INC, l_rule, d_rule, file_cnt) == 0:
-        return 0
 
-def main(args: list[str]) -> int:
+def print_string(list_of_str, d_rule, file_dir) -> int:
+    cnt = 0
+    file_cnt = cnt + 1
+    while cnt != len(list_of_str):
+        if d_rule == 1:
+            with open(f"{file_dir}str{str(file_cnt)}.txt", "w") as new_file:
+                new_file.write(list_of_str[cnt])
+            file_cnt += 1
+        print(list_of_str[cnt])
+        cnt += 1
+    return 0
+
+
+def main() -> int:
     l_rule = 0
     d_rule = 0
+    file_dir = []
     print('Введите -f "путь к файлу" -n "Максимальное количество символов"')
-    if len(sys.argv) == 1:
-         print('Введены недопустимые параметры. Введите -f "путь к файлу" -n "Максимальное количество символов" -l -d')
-         return 0
-    else:
-        param_name = sys.argv[1]
-    if param_name == '-f':
-        FILE_PATH = sys.argv[2]
-    else:
-        print(f'Неизвесный параметр {param_name}')
+    if len(sys.argv) == 1 or sys.argv.index('-f') == -1 or sys.argv.index('-n') == -1:
+        print('Введены недопустимые параметры. Введите -f "путь к файлу" -n "Максимальное количество символов" -l -d')
         return 0
-    param_name = sys.argv[3]
-    if param_name == '-n':
-        max_index = int(sys.argv[4]) + 1
-    else:
-        print(f'Неизвесный параметр {param_name}')
-        return 0
+    file_path = sys.argv[sys.argv.index('-f') + 2]
+    max_cnt = int(sys.argv[sys.argv.index('-n') + 2]) + 1
     if len(sys.argv) > 4:
-        param_name = sys.argv[5]
-        if param_name == '-l':
+        if sys.argv.index('-l') == -1 and sys.argv.index('-d') == -1:
+            print('Введены недопустимые параметры.')
+            return 0
+        if sys.argv.index('-l') != -1:
             l_rule = 1
-        else:
-            print(f'Неизвесный параметр {param_name}')
-            return 0
-    if len(sys.argv) > 5:
-        param_name = sys.argv[6]
-        if param_name == '-d':
+        if sys.argv.index('-d') != -1:
             d_rule = 1
-        else:
-            print(f'Неизвесный параметр {param_name}')
-            return 0
-
-    INDEX_INC = max_index
-    file_cnt = 1
-    with open (sys.argv[2], "r") as file:
+            file_dir = sys.argv[sys.argv.index('-d') + 2]
+    index_inc = max_cnt
+    with open(file_path, "r") as file:
         text = file.read()
     start_index = 0
-    if (processing_and_cropping_str(text, max_index, start_index, INDEX_INC, l_rule, d_rule, file_cnt) == 0):
+    list_of_str = processing_and_cropping_str(text, max_cnt, start_index, index_inc, l_rule)
+    if list_of_str is None:
         return 0
+    print_string(list_of_str, d_rule, file_dir)
+    print('Строки разделены')
 
-if __name__=='__main__':
-    main(sys.argv)
+
+if __name__ == '__main__':
+    main()
