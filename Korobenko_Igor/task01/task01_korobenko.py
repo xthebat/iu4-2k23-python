bf7b3cbb8c8f3ec6731bb0a2c30de0a7c6a69888
igorkorobenko <igorkorobenk@gmail.com>
2023-02-28 23:09:20 +0300

    Add proper task01 + bonus1

--- Korobenko_Igor/task01/task01_korobenko.py
+++ Korobenko_Igor/task01/task01_korobenko.py
@@ -4,18 +4,18 @@ import sys
 def main(args: list[str]):
     read_args = check_param(args)
     split_arr = string_division(read_args)
-    sym_limit = int(read_args[1])
-    result_arr = substring_create(split_arr, sym_limit)
+    result_arr = substring_create(split_arr, int(read_args[1]), read_args[2])
     string_print(result_arr)
 
     return 0
 
-    # АРГУМЕНТЫ ПРОВЕРИТЬ ПО-ДРУГОМУ
-
 
 def check_param(args: list[str]):  # проверить
+    global d_param
     if len(args) < 2:
         sys.exit('ERROR 100: not enough parameters')
+    if len(args) > 5:
+        sys.exit('ERROR 1000: too many parameters')
 
     read_args = []
 
@@ -47,16 +47,20 @@ def check_param(args: list[str]):  # проверить
     except:
         sys.exit('ERROR 1030: no value of -n param')
 
+    read_args.append(False)
+    if '-l' in args:
+        read_args[2] = True
+
     return read_args
 
 
 def string_division(args: list[str]):
     file_name = args[0]
     try:
-        file = open(f"{file_name}")
+        file = open(f"{file_name}", encoding='utf-8')
         text = file.read()
     except FileNotFoundError:
-        sys.exit('ERROR 200: file not found')
+        sys.exit('ERROR 200: file is not found')
 
     # запись строк в массив строк
     split_arr = []
@@ -70,49 +74,71 @@ def string_division(args: list[str]):
     return split_arr
 
 
-def substring_create(split_arr: list[str], sym_limit: int):
+def substring_create(split_arr: list[str], sym_limit: int, flag_l: bool):
     # образование подстрок
     result_arr = ['']
+    # инициализация переменных индекса, номера подстроки и числа символов в рассматриваемой подстроке
     substring_num = 0
     temp_substring_sym = 0
-    # for i in range(0, len(split_arr)):
-    #     if len(split_arr[i]) > sym_limit:
-    #         sys.exit(f"ERROR 300: string #{i + 1} is too long for division\nYou can change -n parameter")
-    #     if (len(result_arr[substring_num]) + len(split_arr[i])) < sym_limit:
-    #         result_arr[substring_num] += split_arr[i] + '\n'
-    #     else:
-    #         substring_num += 1
-    #         result_arr.append(split_arr[i] + '\n')
+    index = 0
 
-    for i in range(0, len(split_arr)):
-        # Если невозможно прибавить всю строку в подстроку, то нужно делить
-        if len(split_arr[i]) + temp_substring_sym > sym_limit:
-            max_addition_len = sym_limit - len(split_arr[i])
-            temp_str = split_arr[i]
-            index = find_separation_sym(split_arr[i], max_addition_len)
-            result_arr[substring_num] += temp_str[0:index] + '\n'
-            result_arr.append('')
-        # Если можно добавить всю строку, то сразу добавляем в подстроку
-        else:
-            result_arr[substring_num] += split_arr[i] + '\n'
-            temp_substring_sym += len(result_arr[substring_num])
+    # Если выставлен флаг -l, то просто делим строки на подстроки с учетом ограничения
+    if flag_l:
+        for i in range(0, len(split_arr)):
+            if len(split_arr[i]) > sym_limit:
+                sys.exit(f"ERROR 300: string #{i + 1} is too long for division\nYou can change -n parameter")
+            if (len(result_arr[substring_num]) + len(split_arr[i])) < sym_limit:
+                result_arr[substring_num] += split_arr[i] + '\n'
+            else:
+                substring_num += 1
+                result_arr.append(split_arr[i] + '\n')
+    else:
+        # иначе проходимся по списку разделенных строк и делим на подстроки по словам
+        for i in range(0, len(split_arr)):
+            # Если невозможно прибавить всю строку в подстроку, то нужно делить строку
+            if len(split_arr[i]) + temp_substring_sym > sym_limit:
+                # определяем максимальное число символов, которое мы можем добавить в подстроку
+                max_addition_len = sym_limit - temp_substring_sym
+                temp_str = split_arr[i]
 
-    result_arr[substring_num] = result_arr[substring_num].removesuffix('\n')
+                # определяем индекс, до которого мы можем скопировать строку в подстроку
+                index = find_separation_sym(split_arr[i], max_addition_len, index)
+                result_arr[substring_num] += temp_str[0:index]
+                result_arr.append('')
+                substring_num += 1
+                temp_substring_sym = 0
+
+                if len(temp_str[index:len(temp_str)]) > temp_substring_sym:
+                    result_arr[substring_num] += temp_str[index:len(temp_str)] + '\n'
+                    index = 0
+                else:
+                    # если строка длинная, то нужно провести вышеописанные операции в цикле несколько раз
+                    i -= 1
+
+            # Если можно добавить всю строку, то сразу добавляем в подстроку
+            else:
+                result_arr[substring_num] += split_arr[i] + '\n'
+
+            # Записываем результирующее значение
+            temp_substring_sym = len(result_arr[substring_num])
+
+        result_arr[substring_num] = result_arr[substring_num].removesuffix('\n')
 
     return result_arr
 
 
-def find_separation_sym(string: str, max_addition_len: int) -> int:
-    index = 0
+def find_separation_sym(string: str, max_addition_len: int, index: int) -> int:
     if string.find('@', index, max_addition_len) != -1:
-        index = string.find('@', index, max_addition_len)
         if string.find(':', index, max_addition_len) != -1:
             index = string.find(':', index, max_addition_len)
-
-    while index < max_addition_len:
-        index = string.find(' ', index, max_addition_len)
-
-    return index
+    else:
+        words = string.split()
+        for word in words:
+            if len(word) + index > max_addition_len:
+                return index
+            else:
+                index += len(word) + 1
+    return index + 1
 
 
 def string_print(result_arr: list[str]):
