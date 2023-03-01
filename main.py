c0a2e3d92bbdf0590cb9bc4fb6b327f4bcc16541
Alexei <garaevalexei@yandex.com>
2023-03-01 15:24:45 +0300

    Работает
    Хочу доработать

--- main.py
+++ main.py
@@ -38,19 +38,44 @@ def find_file(args: list[str]):
     return 0
 
 
-def read_file(filename: str, n: int):
+def write_substrings(string_list: list[str]):
+    for it, string in enumerate(string_list):
+        print('Substring #', it, ":")
+        print(string)
+
+
+def split_string(string: str, n: int):
+    string_list = string.split()
+    string_len = 0
+    melt = 0
+    new_str_list = []
+    new_str = ""
+    for substring in string_list:
+        if substring.find("@") != -1:
+            melt = 1
+
+        if (string_len + len(substring) < n) | melt == 1:
+            string_len = string_len + len(substring) + 1
+            new_str = new_str + ' ' + substring
+        else:
+            new_str_list.append(new_str)
+            new_str = ""
+            string_len = 0
+            
+        if (substring.find(":") != -1) & melt:
+            melt = 0
+    return new_str_list
+
+
+def read_file(filename: str):
     if os.path.getsize(filename) > 0:
-        file = open(filename, 'r')
-        string = file.read(n)
-        inc = 1
-        while len(string) > 0:
-            print('Substring ', inc)
-            inc = inc + 1
-            print(string)
-            string = file.read(n)
+        file = open(filename, encoding="utf8")
+        string = file.readlines()
         file.close()
+        return string
     else:
-        print('file is empty(')
+        print('Файл пуст(')
+        return 0
 
 
 def main():
@@ -58,12 +83,19 @@ def main():
     n = find_n(sys.argv)
     filename = find_file(sys.argv)
 
-    print('chosen file:', filename)
-    print('string\'s length:', n)
+    print('Выбранный файл:', filename)
+    print('Длина строки:', n)
 
-    read_file(filename, n)
+    string_list = []
+    text = ''
+    strings = read_file(filename)
+    for it in strings:
+        text += it
 
-    pass
+    string_splited = split_string(text, n)
+    for i in string_splited:
+        string_list.append(i)
+    write_substrings(string_list)
 
 
 if __name__ == '__main__':
