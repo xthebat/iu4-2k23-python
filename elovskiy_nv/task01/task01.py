7a911ec65bda6209d2c17fab66c23425978d50c3
Sergeysers <elovskiy00@mail.ru>
2023-03-04 11:49:59 +0300

    Fixed and Optimazed Code

--- elovskiy_nv/task01/task01.py
+++ elovskiy_nv/task01/task01.py
@@ -1,103 +1,91 @@
 import sys
 import os.path
 
-# Функция разбиения на подстроки
-def divide(filename: str, lenght: int) -> list[str]:
-    massive = []
-    string = []
-    file = open(filename, encoding='utf-8')
-    pos = 0
-    ind = True
-    startpos = 0
-    endflag = 0
-    lrule = 0
-    text = file.read()
-    file.close()
-    while (ind != False):
-        if endflag == 0:
-            while lenght > len(text[startpos:pos]):
-                if text.find("@", pos) - pos == 1:
-                    taglen = text.find(":", pos) - text.find("@", pos)
-                    if lenght - len(text[startpos:pos]) > taglen:
-                        string = text[startpos:pos]
-                    elif lenght - len(text[startpos:pos]) == taglen:
-                        pos = text.find(":", lastpos)
-                        string = text[startpos:pos]
-                        break
-                    else:
-                        string = text[startpos:pos]
-                        break
-                elif text.find(" ", pos) == -1:
-                    endflag = 1
-                    break
-                else:
-                    string = text[startpos:pos]
-                lastpos = pos
-                pos = text.find(" ", pos + 1)
-            pos = lastpos
-            startpos = pos
-            if len(string) > 0:
-                massive.append(string)
-                string = []
-        else:
-            string = text[pos + 1:len(text)]
-            massive.append(string)
-            ind = False
-            string = []
-            endflag = 0
-            break
-    return massive
+
+# Открытие и чтение файла
+def inputfile(filename: str) -> str:
+    with open(filename, encoding='utf-8') as f:
+        text = f.read()
+    return text
+
 
 # Проверка на максимально длинное слово и возможность его вывода
-def lencheck(filename: str, lenght: int) -> bool:
-    file = open(filename, encoding='utf-8')
-    text = file.read()
+def lencheck(text: str, lenght: int) -> bool:
     lst = text.split()
     wmax = len(max(lst, key=len))
-    file.close()
-    if wmax <= lenght:
-        return True
-    else:
-        return False
+    return wmax <= lenght
+
+
+# Функция разбиения строки на подстроки
+def dividestr(text: str, length: int) -> list[str]:
+    subline = []
+    words = []
+    nowpos = startsub = endflag = 0
+    while True:
+        while length > nowpos - startsub:
+            if text.find(" ", nowpos) == -1:
+                endflag = 1
+                break
+            tagstart = text.find("@", nowpos)
+            tagend = text.find(":", tagstart)
+            taglength = tagend - tagstart
+            if tagstart - nowpos == 1 and length - len(words) == taglength:
+                words = text[startsub:tagend]
+                buff = tagend
+                break
+            elif tagstart - nowpos == 1 and length - len(words) < taglength:
+                break
+            words = text[startsub:nowpos]
+            buff = nowpos
+            nowpos = text.find(" ", nowpos + 1)
+        startsub = nowpos = buff
+        subline.append(words)
+        if endflag:
+            words = text[nowpos + 1:-1]
+            subline.append(words)
+            break
+    return subline
+
 
 # Функция вывода подстрок
-def output(massive: list[str], dcheck: bool, path: str):
-    for i in range(len(massive)):
-        print(f"\nSubstring #{i + 1}:\n{massive[i]}")
-        if dcheck == True:
-            file = open(f"{path}\substring_{i + 1}.txt", "w+")
-            file.write(f"{massive[i]}")
-            file.close()
+def output(subline: list[str], dcheck: bool, path: str):
+    for i, word in enumerate(subline):
+        print(f"\nSubstring #{i + 1}:\n{word}")
+        if dcheck:
+            with open(f"{path}substring_{i + 1}.txt", "w+") as f:
+                f.write(f"{subline[i]}")
+    return 0
 
-def main(args: list[str]):
 
-    # Определение наличия аргумента -d
-    if len(args) > 3 and (args[3] == "-d" or args[5] == "-d"):
-        dflag = True
-    else:
-        dflag = False
+def main():
+    numb = 200
+    dflag = False
 
-    # Определение наличия аргумента -n
-    if len(args) > 3 and args[3] == "-n":
-        numb = int(args[4])
-    else:
-        numb = 200
+    # Определение положения аргументов и их значений
+    for i in range(len(sys.argv)):
+        match sys.argv[i]:
+            case '-f':
+                fpath = sys.argv[i + 1]
+            case '-n':
+                numb = int(sys.argv[i + 1])
+            case '-d':
+                dflag = True
+                dpath = sys.argv[i + 1]
 
-    if args[1] == "-f" and len(args) >= 3:
-        if os.path.isfile(args[2]) and lencheck(args[2], numb) == True:
-            outputdate = divide(args[2], numb)
-            if args[3] == "-d":
-                output(outputdate, dflag, args[4])
-            elif args[5] == "-d":
-                output(outputdate, dflag, args[6])
-            else:
-                output(outputdate, dflag)
-        elif os.path.isfile(args[2]) and lencheck(args[2], numb) == False:
-            print(f"Слишком маленькое значение для разбиения")
+    text = inputfile(fpath)
+    if os.path.isfile(fpath) and lencheck(text, numb):
+        outputdate = dividestr(text, numb)
+        if dflag:
+            output(outputdate, dflag, dpath)
         else:
-            print(f"Некорректное название файла/директории")
+            output(outputdate, dflag)
+    elif not lencheck(text, numb):
+        print(f"Слишком маленькое значение для разбиения")
+    elif not os.path.isfile(fpath):
+        print(f"Некорректное название файла/директории")
     else:
         print(f"Некорректно введенные данные")
 
+
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(main())
