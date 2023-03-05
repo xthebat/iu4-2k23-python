449df3e7937ad38c406782985ccf6a5da2e80049
Arthur <artyr-xamidullin@mail.ru>
2023-03-05 23:00:17 +0300

    Исправил недочеты Добавил bonus 1 и 2

--- khamidullin_ar/main.py
+++ khamidullin_ar/main.py
@@ -36,6 +36,11 @@ def args_handler(args: list[str]) -> bool or dict:
     else:
         args_values["-d"] = False
 
+    if '-l' in args:
+        args_values["-l"] = True
+    else:
+        args_values["-l"] = False
+
     return args_values
 
 
@@ -74,31 +79,86 @@ def convert_tokens_to_substrings(tokens: list[str], n: int) -> list[str] or bool
     return sub_strings
 
 
+def search_newscore_strings_pos_len(words: list[str]) -> tuple:
+    position = []
+    length = []
+    i = 0
+    while i < len(words):
+        if words[i] == ('-') and words[i + 1] == ('Новый'):
+            position.append(i)
+            k = i
+            while k < len(words):
+                if words[k].endswith('\n'):
+                    length.append(k - i)
+                    break
+                k += 1
+        i += 1
+    return position, length
+
+
+def convert_newscore_strings_to_connected_tokens(position: list[int], length: list[int], words: list[str]) -> list[str]:
+    tokens = []
+    buffer = []
+    i = k = j = 0
+    while i < len(words):
+        if i == position[j]:
+            while k <= length[j]:
+                buffer.append(words[i])
+                k += 1
+                i += 1
+            k = 0
+            tokens.append(' '.join(buffer))
+            buffer.clear()
+            j += 1
+            i -= 1
+            if j == len(position):
+                j = -1
+        else:
+            tokens.append(words[i])
+        i += 1
+
+    return tokens
+
+
+def print_in_console_and_file(string: str, sub_strings: list[str], args_values: dict):
+    # Убираем лишние пробелы
+    formatted_string = re.sub(' \n ', '\n', string)
+    if args_values["-d"]:
+        with open('substring_{}.txt'.format(sub_strings.index(string) + 1), "w") as file:
+            file.write(formatted_string)
+    print('Substring #{}:'.format(sub_strings.index(string) + 1))
+    if args_values["-l"]:
+        print(f'{formatted_string}')
+    else:
+        print(f'{formatted_string}\n')
+
+
 def main(args: list[str]) -> int:
     args_values = args_handler(args)
 
     if args_values:
         with open(args_values["-f"], 'rt', encoding="utf-8") as f:
-            words = f.read().split(' ')
+            text = f.read()
+            words = re.findall(r'[^ \n]+|\n', text)
     else:
         print("Что-то пошло не так, советую ознакомиться с документацией :)")
         return -1
 
-    tokens = convert_text_to_tokens(words)
-    sub_strings = convert_tokens_to_substrings(tokens, args_values["-n"])
+    if args_values["-l"]:
+        position, length = search_newscore_strings_pos_len(words)
+        connected_tokens = convert_newscore_strings_to_connected_tokens(position, length, words)
+        sub_strings = convert_tokens_to_substrings(connected_tokens, args_values["-n"])
+    else:
+        tokens = convert_text_to_tokens(words)
+        sub_strings = convert_tokens_to_substrings(tokens, args_values["-n"])
 
+    # Проверка что не вернулось значение False
     if sub_strings:
         for string in sub_strings:
-            print('Substring #{}:'.format(sub_strings.index(string) + 1))
-            print(f'{string}\n')
+            print_in_console_and_file(string, sub_strings, args_values)
     else:
         print("Некоторые слова или токены больше значения аргумента n и не могут быть разделены")
         return -1
-
-    if args_values["-d"]:
-        for string in sub_strings:
-            with open('substring_{}.txt'.format(sub_strings.index(string) + 1), "w") as file:
-                file.write(string)
 
     return 0
 
