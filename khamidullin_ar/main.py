5eda7c52e58b08f1d9d38ffa81b53cb98be44a7e
Arthur <artyr-xamidullin@mail.ru>
2023-03-05 20:57:48 +0300

    Исправил замечания Добавил Bonus 2

--- khamidullin_ar/main.py
+++ khamidullin_ar/main.py
@@ -2,16 +2,17 @@ import sys
 import re
 
 
-def file_handler(args: list[str]) -> bool or dict:
+def args_handler(args: list[str]) -> bool or dict:
     args_values = {}
 
     if '-f' in args:
         f_pos = args.index('-f')
+        # Проверка, что после аргумента -f действительно записан путь к файлу или имя файла
         if re.match('(\w:.+)|(.+\.\w+)', args[f_pos + 1]):
             f_name = args[f_pos + 1]
             args_values["-f"] = f"{f_name}"
         else:
-            print("Отсутствует путь файла или его имя, если он расположен в корне")
+            print("Отсутствует, либо введены неверно     путь файла или его имя, если он расположен в корне")
             return False
     else:
         print("Отсутствует аргумент -f")
@@ -19,6 +20,7 @@ def file_handler(args: list[str]) -> bool or dict:
 
     if '-n' in args:
         n_pos = args.index('-n')
+        # Проверка, что после -n действительно записано число
         if re.match('\d+', args[n_pos + 1]):
             n_value = args[n_pos + 1]
             args_values["-n"] = int(n_value)
@@ -28,27 +30,34 @@ def file_handler(args: list[str]) -> bool or dict:
     else:
         print("Отсутствует аргумент -n")
         return False
+
+    if '-d' in args:
+        args_values["-d"] = True
+    else:
+        args_values["-d"] = False
+
     return args_values
 
 
-def text_to_tokens(words: str) -> list[str]:
+def convert_text_to_tokens(words: list[str]) -> list[str]:
     tokens = []
     i = 0
     while i < len(words):
-        if words[i].startswith('@'):
-            if words[i].endswith(':') or words[i].endswith(':/n'):
-                tokens.append(words[i])
+        word_i = words[i]
+        if word_i.startswith('@'):
+            if word_i.endswith(':') or word_i.endswith('/n'):
+                tokens.append(word_i)
                 i += 1
             else:
-                tokens.append(words[i] + ' ' + words[i + 1])
+                tokens.append(word_i + ' ' + words[i + 1])
                 i += 2
         else:
-            tokens.append(words[i])
+            tokens.append(word_i)
             i += 1
     return tokens
 
 
-def tokens_to_substrings(tokens: list[str], n: int) -> list[str] or bool:
+def convert_tokens_to_substrings(tokens: list[str], n: int) -> list[str] or bool:
     sub_strings = []
     buffer = []
     for token in tokens:
@@ -66,17 +75,17 @@ def tokens_to_substrings(tokens: list[str], n: int) -> list[str] or bool:
 
 
 def main(args: list[str]) -> int:
-    args_values = file_handler(args)
+    args_values = args_handler(args)
 
     if args_values:
-        with open(args_values["-f"], 'rt') as f:
+        with open(args_values["-f"], 'rt', encoding="utf-8") as f:
             words = f.read().split(' ')
     else:
         print("Что-то пошло не так, советую ознакомиться с документацией :)")
         return -1
 
-    tokens = text_to_tokens(words)
-    sub_strings = tokens_to_substrings(tokens, args_values["-n"])
+    tokens = convert_text_to_tokens(words)
+    sub_strings = convert_tokens_to_substrings(tokens, args_values["-n"])
 
     if sub_strings:
         for string in sub_strings:
@@ -85,6 +94,11 @@ def main(args: list[str]) -> int:
     else:
         print("Некоторые слова или токены больше значения аргумента n и не могут быть разделены")
         return -1
+
+    if args_values["-d"]:
+        for string in sub_strings:
+            with open('substring_{}.txt'.format(sub_strings.index(string) + 1), "w") as file:
+                file.write(string)
 
     return 0
 
