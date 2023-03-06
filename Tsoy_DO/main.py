4c11fa256dac2e3573f9667a3571516b0d9d27fd
Dima <dmitry.tsoy@yandex.ru>
2023-03-06 22:20:01 +0300

    fixed errors
    fixed errors

--- Tsoy_DO/main.py
+++ Tsoy_DO/main.py
@@ -1,94 +1,90 @@
-import sys
 import os.path
+import sys
 
 
-def Fun_out(text_slice, k):
+def print_chunk(text_slice: str, k: int):
     print("Substring #", k, ":")
     print(f"{text_slice}")
 
 
-def Check_f(args: list[str]) -> int:
-    adr = 0
+def parse_parameter_f(args: list[str]) -> str:
+    flag = 0
     if len(args) < 2:
         sys.exit("Not enough")
-    if args[1] == "-f":
-        adr = args[2]
-    else:
-        sys.exit("I have not -f parametr")
-
+    for i in range(0, len(args), 1):
+        if args[i] == '-f':
+            adr = args[i+1]
+            flag = 1
+    if flag == 0:
+        sys.exit("I have not -f parameter")
     if not os.path.exists(adr):
         sys.exit("File is not found!")
     return adr
 
 
-def Check_n(args: list[str]) -> int:
-    kol = 200
-    if args[3] == "-n":
-        kol = int(args[4])
-    else:
-        print("I have not -n parametr, I will use 200")
-    return kol
+def parse_parameter_n(args: list[str]) -> int:
+    number = 200
+    flag = 0
+    for i in range(0, len(args), 1):
+        if args[i] == '-n':
+            number = int(args[i+1])
+            flag = 1
+    if flag == 0:
+        print("I have not -n parameter, I will use 200")
+    return number
 
 
-def Check_tag(text, i, last):
-    sobaka = -1
-    for j in range(i, last+1, 1):
-        if (text[j] == '@'):
-            sobaka = j
+def check_tag(text, i, last) -> int:
+    at_symbol = -1
+    for j in range(i, last + 1, 1):
+        if text[j] == '@':
+            at_symbol = j
     f_colon = -1
-    if (sobaka != -1):
-        for j in range(sobaka, last+1, 1):
-            if (text[j] == ':'):
+    if at_symbol != -1:
+        for j in range(at_symbol, last + 1, 1):
+            if text[j] == ':':
                 f_colon = 1
-    if f_colon == -1 and sobaka != -1:
-        last = sobaka - 1
-        if sobaka == i:
+    if f_colon == -1 and at_symbol != -1:
+        last = at_symbol - 1
+        if at_symbol == i:
             sys.exit("user-friendly")
-
-
-def Sub_reduction(text, i, last) -> int:
-    while text[last] != ' ' and text[last+1] != ' ' and text[last] != '\n' and text[last+1] != '\n':
-        last = last - 1
-        if last < i:
-            sys.exit("user-friendly")
-
     return last
 
 
-def Fun_del(text: list[str], kol):
+def sub_reduction(text, i, last) -> int:
+    while text[last] != ' ' and text[last + 1] != ' ' and text[last] != '\n' and text[last + 1] != '\n':
+        last = last - 1
+        if last < i:
+            sys.exit("user-friendly")
+    return last
+
+
+def fun_del(text: str, kol: int):
     count = len(text)
     k = 1
     i = 0
     if count == 0:
         print("File is empty!")
-
-    while i <= count-1:
-        if i+kol-1 <= count-1:
-            last = i+kol-1
+    while i <= count - 1:
+        if i + kol - 1 <= count - 1:
+            last = i + kol - 1
         else:
-            last = count-1
-
-        if last != count-1:
-            last = Sub_reduction(text, i, last)
-
-        Check_tag(text, i, last)
-
-        text_slice = text[i:last+1]
-        Fun_out(text_slice, k);
+            last = count - 1
+        if last != count - 1:
+            last = sub_reduction(text, i, last)
+        check_tag(text, i, last)
+        text_slice = text[i:last + 1]
+        print_chunk(text_slice, k)
         k = k + 1
         i = last + 1
 
 
 def main() -> int:
-    adr = Check_f(sys.argv)
-    kol = Check_n(sys.argv)
-
-    file = open(adr, "rt", encoding='utf-8')
-    text = file.read()
-    file.close()
-
-    Fun_del(text, kol)
-
+    adr = parse_parameter_f(sys.argv)
+    kol = parse_parameter_n(sys.argv)
+    with open(adr, "rt", encoding='utf-8') as file:
+        text = file.read()
+    fun_del(text, kol)
     return 0
 
 
