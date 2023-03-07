fb812800c6139e8426ac8f72f22d0246318448a0
i.umitbaev <umitbaeva.alsu@mail.ru>
2023-03-07 20:10:44 +0300

    fixup! Add new files

--- umitbaev_is/main.py
+++ umitbaev_is/main.py
@@ -3,95 +3,92 @@ import os
 import pathlib
 
 
-def main(par):
-    check_path(par.file)
-    text = read_file(par.file)
-    dog_list = find_all_dogs(text)
-    flag, result_list = divide_string(text, dog_list, par.number)
-    result(flag, result_list)
+def main():
+    parser = argparse.ArgumentParser(description="Enter arguments")
+    parser.add_argument('-n', '--number', type=int, default=200, help="Enter '-n' to set number of symvols i")
+    parser.add_argument('-f', '--file', type=str, help="Enter path to file works only with '-f' in front")
+    parser.add_argument('-d', '--direct', type=str, help="Enter path to file works only with '-f' in front")
+
+    args = parser.parse_args()
+
+    check_path(args.file)
+    text = read_file(args.file)
+    divide_index = divide_string(text, args.number)
+
+    create_files(args.direct, divide_index, text)
+
+    print_strings(divide_index, text)
 
 
 def check_path(path):
     file = pathlib.Path(path)
-    if not file.is_file():
+    if not file.is_file() or not os.stat(path).st_size:
         print("No such file")
-        exit(0)
+        exit(1)
     if not os.stat(path).st_size:
         print("File is empty")
-        exit(0)
+        exit(1)
 
 
 def read_file(path):
     with open(path, "r", encoding="utf-8") as file:
         text = file.read()
-    return text + " "
+    return text
 
 
-def find_all_dogs(text):
-    dog_list = []
-    final_dog_list = []
+def divide_string(text: str, max_number: int) -> list[int]:
+    last_index = 0
+    divide_index = []
     flag = False
-    for i in range(len(text)):
-        if text[i] == "@":
+    result_len = 0
+    for index in range(len(text)):
+        if text[index] == '@':
             flag = True
-            dog_list.append(i)
-        if flag is True and text[i] == ":":
-            dog_list.append(i)
+        if text[index] == ':':
             flag = False
-    for i in range(1, len(dog_list), 2):
-        final_dog_list.append((dog_list[i - 1], dog_list[i]))
-    return final_dog_list
+        if not flag and text[index].isspace():
+            if index - result_len > max_number:
+                if last_index == 0 or last_index in divide_index:
+                    return []
+                divide_index.append(last_index)
+                result_len = last_index
+            else:
+                last_index = index
+
+    if len(text) - divide_index[-1] > max_number:
+        divide_index.append(last_index)
+
+    divide_index.append(len(text))
+    return divide_index
 
 
-def divide_string(text, dog_list, number):
-    result_list = []
-    start_index = 0
-    error_string = False
-
-    while start_index < len(text) - 1:
-        end = start_index + number
-        if end > len(text):
-            end = len(text) - 1
-        if (text[end] == " " or text[end] == "\n") and check_between(end, dog_list):
-            result_list.append(text[start_index:end + 1])
-            start_index = end + 1
-        else:
-            stop = False
-            while not stop:
-                end = text.rfind(" ", start_index, end)
-                if end == -1:
-                    stop = True
-                    error_string = True
-                    start_index = len(text)  # to exit 'while' immediately
-                else:
-                    if check_between(end, dog_list):
-                        result_list.append(text[start_index:end + 1])
-                        start_index = end + 1
-                        stop = True
-
-    return error_string, result_list
+def create_files(direct: str, divide_index: list[int], text: str):
+    if direct is not None:
+        if not direct.endswith('\\'):
+            direct = direct + '\\'
+        try:
+            print_into_file(text, divide_index, direct)
+        except FileNotFoundError:
+            print(f"Вы указали директорию, которой не существует:{direct}")
+            exit(1)
 
 
-def check_between(end, dog_list):
-    norm = True
-    for k in dog_list:
-        if k[0] < end < k[1]:
-            norm = False
-            break
-    return norm
+def print_into_file(text: str, divide_index: list[int], direct: str):
+    start = 0
+    for index, end in enumerate(divide_index, 1):
+        test_file = open(f'{direct}substring_{index}.txt', 'w')
+        test_file.write(f"Substring: #{index}:\n{text[start:end]}\n")
+
+        start = end + 1
 
 
-def result(flag, result_list):
-    if not flag:
-        for i, string in enumerate(result_list):
-            print(f"Substring {i + 1}:\n{string}\n")
-    else:
-        print("Error in dividing substring")
+def print_strings(divide_index: list[int], string: str):
+    start = 0
+    for index, end in enumerate(divide_index, 1):
+        print(f"Substring: #{index}:")
+        print(string[start:end], "\n")
+        start = end + 1
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Enter arguments")
-    parser.add_argument('-n', '--number', type=int, default=200, help="Enter '-n' to set number of symvols i")
-    parser.add_argument('-f', '--file', type=str, help="Enter path to file works only with '-f' in front")
-    args = parser.parse_args()
-    main(args)
+    main()
