9c7d21f14150603e7f36f487d51a7cf35905795d
obrubovs <siobrubov@yandex.ru>
2023-03-08 02:25:43 +0300

    Fixed some variable-names, added "click" library for parsing function, inverted some if-else constructions

--- obrubov_si/python_project1/main.py
+++ obrubov_si/python_project1/main.py
@@ -1,44 +1,35 @@
 import sys
 import os.path
 import re
+import click
 
 
-def main(args: list[str]):
-    # проверка наилчия арументов
-    if len(args) < 2:
-        print(f" not enough {len(args)=}: required > 2")
+@click.command()
+@click.option("-n", "subst_length", default=200, help="Max symbols in substring")
+@click.option("-f", "file_path", required=True, help="Max symbols in substring")
+def main(subst_length: int, file_path: str):
+
+    if not os.path.isfile(file_path):
+        print("incorrect file path")
         return -1
 
-    # проверка  аргумента -n
-    if len(args) > 3 and args[3] == "-n":
-        substr_length = int(args[4])
-    else:
-        substr_length = 200
+    file = open(file_path, encoding='utf-8')
+    text = file.read()
+    file.close()
+    listed_text = re.findall(r'\S+|\n', text)
+    text_with_logins = login_detector(listed_text)
+    list_to_print = string_divider(text_with_logins, subst_length)
+    output(list_to_print)
 
-    # проверка -f пути до файла
-    if args[1] == "-f" and len(args) > 2:
-        if os.path.isfile(args[2]):
-            file_path = args[2]
-            file = open(file_path, encoding='utf-8')
-            text = file.read()
-            file.close()
-            listed_text = re.findall(r'\S+|\n', text)
+
+def login_detector(input_list: list[str]) -> list[str]:
+    output_list = []
+    for i in range(len(input_list)):
+        if input_list[i].startswith('@') and input_list[i + 1].endswith(':'):
+            input_list[i + 1] = input_list[i] + ' ' + input_list[i + 1]
         else:
-            print("incorrect file path")
-    else:
-        print(f"Incorrect input data")
-
-    list_to_print = string_divider(login_detector(listed_text), substr_length)
-
-
-def login_detector(listed_text: list[str]) -> list[str]:
-    newlist = []
-    for i in range(len(listed_text)):
-        if listed_text[i].startswith('@') and listed_text[i + 1].endswith(':'):
-            listed_text[i + 1] = listed_text[i] + ' ' + listed_text[i + 1]
-        else:
-            newlist.append(listed_text[i])
-    return newlist
+            output_list.append(input_list[i])
+    return output_list
 
 
 def string_divider(listed_string: list[str], max_length: int) -> list[str]:
@@ -46,11 +37,7 @@ def string_divider(listed_string: list[str], max_length: int) -> list[str]:
     string = []
     sorted_text = []
     for i in range(len(listed_string)):
-
-        if listed_string[i] == '\n':
-            element_length = 0
-        else:
-            element_length = len(listed_string[i])
+        element_length = 0 if listed_string[i] == '\n' else len(listed_string[i])
 
         if string_length + element_length + len(string) - string.count('\n') <= max_length:
             string_length += element_length
@@ -72,7 +59,7 @@ def string_divider(listed_string: list[str], max_length: int) -> list[str]:
     return sorted_text
 
 
-def output(sorted_text: list[str], d: bool, path: str):
+def output(sorted_text: list[str]):
     for i in range(len(sorted_text)):
         print(f"\nSubstring #{i + 1}:\n{sorted_text[i]}")
 
