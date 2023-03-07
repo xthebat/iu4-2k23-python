0ec7e0dc5fd306c58d7af80ab9c9a4c08f1af2e4
Bandix123 <56293436+Bandix123@users.noreply.github.com>
2023-03-07 18:08:12 +0300

    Update Bondarenko_TASK1.py

--- Bondarenko_TASK1.py
+++ Bondarenko_TASK1.py
@@ -1,77 +1,112 @@
 # Bondarenko UI4-82B
+# With bonus 1
 
 import sys
+import re
 
 
-def glue_nickname(file):
+def glue_nickname(words: list[str]):
     pop_numbers = []
-    b = 0
-    k = 1
-    for j in range(len(file)):
-        if file[j][0:1] == "@":
-            file[j] = file[j] + ' ' + file[j + 1]
-            pop_numbers.append(j + 1)
-            b += 1
-    while b > 0:
-        # array_by_word.pop(pop_numbers[1 - k])
-        q = pop_numbers[0 - k]
-        file.pop(q)
-        # print(pop_numbers[0 - k])
-        k += 1
-        b = b - 1
+    start_num = 0
 
-    return file
+    k = 1
+    for j, word in enumerate(words):
+        # print(word)
+        if word.startswith('@'):
+            start_num = j
+        if word.endswith(':') and j > start_num:
+            end_num = j
+            if end_num > start_num > 0:
+                # print('YES')
+                for i in range(start_num + 1, end_num + 1):
+                    words[start_num] = words[start_num] + ' ' + words[i]
+                    pop_numbers.append(i)
+    b = len(pop_numbers)
+    while b > 0:
+        q = pop_numbers[-k]
+        words.pop(q)
+        k += 1
+        b -= 1
+
+    return 0
+
+
+def l_param_glue(words: list[str], l_param: str):
+    pop_numbers = []
+    start_num = 0
+    k = 1
+    if l_param == '-l':
+        for j, word in enumerate(words):
+            # print(word)
+            if word.startswith('-'):
+                start_num = j
+            if word.endswith(':') and j > start_num:
+                end_num = j
+                if end_num > start_num > 0:
+                    # print('YES')
+                    for i in range(start_num + 1, end_num + 1):
+                        words[start_num] = words[start_num] + ' ' + words[i]
+                        pop_numbers.append(i)
+    b = len(pop_numbers)
+    while b > 0:
+        q = pop_numbers[-k]
+        words.pop(q)
+        k += 1
+        b -= 1
+
+    return 0
+
+
+def divide_substring(words: list[str], n_param: int):
+    count = int(n_param)
+    n = 1
+    print('\n' + f"Substring #{n}")
+
+    for i, word in enumerate(words):
+        if count <= (len(word)):
+            count = int(n_param)
+            n += 1
+            print('\n' + f"Substring #{n}")
+            print(word, end=' ')
+            i -= 1
+            count = count - len(word) - 1
+        else:
+
+            if word == '\n':
+                count = count - len(word)
+            else:
+                count = count - len(word) - 1
+            print(word, end=' ')
+            i += 1
+    return 0
 
 
 def main(args: list[str]) -> int:
+    f_param = '\0'
+    n_param = 0
+    l_param = '\0'
+    for i in range(len(args)):
+        if args[i] == "-f":
+            print(f"I have -f parameter={args[i + 1]}")
+            f_param = args[i + 1]
 
-    if args[1] == "-f":
-        print(f"I have -f parameter={args[2]}")
-    else:
-        print("I have not -f parameter")
+        if args[i].isdigit() and int(args[i]) > 0:
+            print(f"-n parameter is correct={args[i]}")
+            n_param = args[i]
 
-    if args[4].isdigit() and int(args[4]) > 0:
-        print("-n parameter is correct")
-    else:
-        print('So sad, incorrect -n  parameter')
-        exit()
+        if args[i] == "-l":
+            print(f"I have -l parameter={args[i]}")
+            l_param = args[i]
+    with open(f_param, "rt", encoding="utf-8") as file:
+        text = file.read()
+        array_by_word = re.findall(r'[^ \n]+|\n', text)
 
-    # if file.isfile('./input_file.txt'):
-    file = open(args[2], "rt", encoding="utf-8")
-    # else:
-    #     print('So sad, so sad')
-
-    text = file.read()
-    # считали файл
-
-    import re
-    array_by_word = re.findall(r'[^ \n]+|\n', text)
-
-    n = 1
-    count = int(args[4])
     glue_nickname(array_by_word)
-    # print(array_by_word)
+    l_param_glue(array_by_word, l_param)
+    divide_substring(array_by_word, int(n_param))
 
-    print('\n' + "Substring #" + str(n))
-    for i in range(len(array_by_word)):
-        # i - кол слов
-        if count <= (len(array_by_word[i])):
-            count = int(args[4])
-            n += 1
-            print('\n' + "Substring #" + str(n))
-            print(array_by_word[i], end=' ')
-            i = i - 1
-            count = count - len(array_by_word[i]) - 1
-        else:
-
-            if array_by_word[i] == '\n':
-                count = count - len(array_by_word[i])
-            else:
-                count = count - len(array_by_word[i]) - 1
-            print(array_by_word[i], end=' ')
-            i += 1
-
-    file.close()
+    print(array_by_word)
+    # file.close()
     print('\n\n')
     return 0
 
