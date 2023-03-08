802022ad4ce3727722b00f618669f61b63aa3d28
igorkorobenko <igorkorobenk@gmail.com>
2023-03-08 14:38:06 +0300

    Add corrections (no corrections to substring_create)

--- Korobenko_Igor/task01/task01_korobenko.py
+++ Korobenko_Igor/task01/task01_korobenko.py
@@ -1,82 +1,95 @@
 import sys
 
 
-def main(args: list[str]):
+def main(args: list[str]) -> int:
     read_args = check_param(args)
+    error_print(read_args)
     split_arr = string_division(read_args)
+    if split_arr == -1:
+        sys.exit('ERROR: file is not found')
     result_arr = substring_create(split_arr, int(read_args[1]), read_args[2])
     string_print(result_arr)
 
     return 0
 
 
-def check_param(args: list[str]):  # проверить
-    global d_param
-    if len(args) < 2:
-        sys.exit('ERROR 100: not enough parameters')
-    if len(args) > 5:
-        sys.exit('ERROR 1000: too many parameters')
+def check_param(args: list[str]) -> list[str | bool | int]:
+    min_len = 2
+    max_len = 5
+    if len(args) < min_len:
+        sys.exit('ERROR: not enough parameters')
+    if len(args) > max_len:
+        sys.exit('ERROR: too many parameters')
 
     read_args = []
 
-    # Проверка f параметра
     if '-f' in args:
         f_param = args.index('-f')
+        try:
+            f_path_param = args[f_param + 1]
+            read_args.append(f_path_param)
+        except:
+            read_args.append(False)
     else:
-        sys.exit('ERROR 101: no "-f" parameter')
-
-    try:
-        f_path_param = args[f_param + 1]
-        read_args.append(f_path_param)
-    except:
-        sys.exit('ERROR 1010: no file path')
-
-    if f_path_param.find('.txt', 0, len(f_path_param)) == -1:
-        sys.exit('ERROR 102: file has no .txt extension')
-
-    # Проверка -n параметра
+        read_args.append(False)
 
     if '-n' in args:
         n_param = args.index('-n')
+
+        try:
+            n_num = args[n_param + 1]
+            read_args.append(n_num)
+        except:
+            read_args.append(False)
     else:
-        sys.exit('ERROR 103: no "-n" parameter')
-
-    try:
-        n_num = args[n_param + 1]
+        n_num = -1
         read_args.append(n_num)
-    except:
-        sys.exit('ERROR 1030: no value of -n param')
+        print('There will be no strict symbol number substrings')
 
-    read_args.append(False)
-    if '-l' in args:
-        read_args[2] = True
+    is_l_argument_appeared = True if '-l' in args else False
+    read_args.append(is_l_argument_appeared)
 
     return read_args
 
+    # read_args: [-f val] [-n_val] [-l_val]
 
-def string_division(args: list[str]):
+
+def error_print(args: list[str | bool]) -> None:
+    if not args[0]:
+        sys.exit('ERROR: no "-f" parameter')
+    if not args[0].endswith('.txt'):
+        sys.exit('ERROR: file has no .txt extension')
+    if not args[1]:
+        return None
+    else:
+        if int(args[1]) < -1:
+            sys.exit('ERROR: invalid -n parameter value')
+    return None
+
+
+def string_division(args: list[str]) -> int | list[str]:
     file_name = args[0]
     try:
-        file = open(f"{file_name}", encoding='utf-8')
+        file = open(file_name, encoding='utf-8')
         text = file.read()
-    except FileNotFoundError:
-        sys.exit('ERROR 200: file is not found')
-
-    # запись строк в массив строк
-    split_arr = []
-    for line in text.splitlines():
-        split_arr.append(line)
+    except:
+        return -1
     file.close()
 
-    if split_arr[len(split_arr) - 1] == '\n':
-        del split_arr[len(split_arr)]
+    # запись строк в массив строк
+    split_arr = text.splitlines()
 
     return split_arr
 
 
-def substring_create(split_arr: list[str], sym_limit: int, flag_l: bool):
+def substring_create(split_arr: list[str], sym_limit: int, flag_l: bool) -> list[str]:
     # образование подстрок
     result_arr = ['']
+    if sym_limit == -1:
+        for string in split_arr:
+            result_arr[0] += string + '\n'
+        result_arr[0].removesuffix('\n')
+        return result_arr
     # инициализация переменных индекса, номера подстроки и числа символов в рассматриваемой подстроке
     substring_num = 0
     temp_substring_sym = 0
@@ -86,7 +99,7 @@ def substring_create(split_arr: list[str], sym_limit: int, flag_l: bool):
     if flag_l:
         for i in range(0, len(split_arr)):
             if len(split_arr[i]) > sym_limit:
-                sys.exit(f"ERROR 300: string #{i + 1} is too long for division\nYou can change -n parameter")
+                sys.exit(f"ERROR: string #{i + 1} is too long for division\nYou can change -n parameter")
             if (len(result_arr[substring_num]) + len(split_arr[i])) < sym_limit:
                 result_arr[substring_num] += split_arr[i] + '\n'
             else:
@@ -102,7 +115,7 @@ def substring_create(split_arr: list[str], sym_limit: int, flag_l: bool):
                 temp_str = split_arr[i]
 
                 # определяем индекс, до которого мы можем скопировать строку в подстроку
-                index = find_separation_sym(split_arr[i], max_addition_len, index)
+                index = find_separation_symbol(split_arr[i], max_addition_len, index)
                 result_arr[substring_num] += temp_str[0:index]
                 result_arr.append('')
                 substring_num += 1
@@ -114,7 +127,7 @@ def substring_create(split_arr: list[str], sym_limit: int, flag_l: bool):
                 else:
                     # если строка длинная, то нужно провести вышеописанные операции в цикле несколько раз
                     i -= 1
- 
+
             # Если можно добавить всю строку, то сразу добавляем в подстроку
             else:
                 result_arr[substring_num] += split_arr[i] + '\n'
@@ -127,7 +140,7 @@ def substring_create(split_arr: list[str], sym_limit: int, flag_l: bool):
     return result_arr
 
 
-def find_separation_sym(string: str, max_addition_len: int, index: int) -> int:
+def find_separation_symbol(string: str, max_addition_len: int, index: int) -> int:
     if string.find('@', index, max_addition_len) != -1:
         if string.find(':', index, max_addition_len) != -1:
             index = string.find(':', index, max_addition_len)
@@ -142,8 +155,8 @@ def find_separation_sym(string: str, max_addition_len: int, index: int) -> int:
 
 
 def string_print(result_arr: list[str]):
-    for i in (range(0, len(result_arr))):
-        print(f'Substring #{i + 1}\n{result_arr[i]}')
+    for i, substr in enumerate(result_arr):
+        print(f'Substring #{i + 1}\n{substr}')
 
 
 if __name__ == '__main__':
