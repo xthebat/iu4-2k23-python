3c9b3daacb89c7be6875232466704bea900e2323
KrawezG <56290476+KrawezG@users.noreply.github.com>
2023-03-10 21:25:05 +0300

    Add files via upload

---
+++ Kravets_GI/main.py
@@ -0,0 +1,98 @@
+import sys
+import os
+
+
+def check_length(text: str, length: int, l_index: int) -> bool:
+    if l_index == -1:
+        list_ = text.split(" ")
+        sorted_list = sorted(list_, key=len)
+        return len(sorted_list[-1]) < length
+    else:
+        list_ = text.split("\n")
+        sorted_list = sorted(list_, key=len, reverse=True)
+        while sorted_list:
+            if sorted_list.stratswith("    -"):
+                return len(sorted_list) < length
+
+
+def divide_strings(text: str, length: int, l_index: int) -> list[str]:
+    splinted_text = list(text.split(" "))
+    current_substring = ""
+    tag = ""
+    substrings = []
+    flag = 0
+    for i in splinted_text:
+        if len(current_substring) < length:
+            if flag == 0:
+                if i.startswith("@"):
+                    flag = 1
+                    tag = i
+                    if i.startswith(":"):
+                        flag = 0
+                        if len(current_substring) + len(tag) <= length:
+                            current_substring += " " + tag
+                        else:
+                            substrings.append(current_substring)
+                            current_substring = tag
+                else:
+                    if len(current_substring) + len(i) <= length:
+                        current_substring += " " + i
+                    else:
+                        substrings.append(current_substring)
+                        current_substring = i
+            else:
+                tag += " " + i
+                if i[-1] == ":":
+                    flag = 0
+
+                    if len(current_substring) + len(tag) <= length:
+                        current_substring += " " + tag
+                    else:
+                        substrings.append(current_substring)
+                        current_substring = tag
+
+    if len(current_substring) != 0:
+        substrings.append(current_substring)
+
+    return substrings
+
+
+def print_strings(strings: list[str]):
+    for i in range(len(strings)):
+        print(f"Substring #{i+1}:\n{strings[i]}")
+
+
+def main(args: list[str]) -> int:
+    length = 200
+    if len(args) < 3:
+        print(f"Not enough {len(args)=} required at least filepath")
+        return -1
+    else:
+        f_index = args.index("-f")
+        if f_index == -1:
+            print("No filepath found")
+            return -1
+        elif not os.path.isfile(args[f_index+1]):
+            print("Incorrect path to file")
+            return -1
+        else:
+            with open(sys.argv[f_index+1], encoding='utf-8') as file:
+                text = file.read()
+        l_index = args.index("-l")
+        n_index = args.index("-n")
+        if n_index != -1 and args[n_index+1].isdigit:
+            if check_length(text, int(args[n_index+1]), l_index):
+                length = int(sys.argv[n_index+1])
+            else:
+                print("Length too small , unable divide text")
+                return -1
+        elif not args[n_index+1].isdigit:
+            print("Incorrect length value")
+            return -1
+        substrings = divide_strings(text, length, l_index)
+        print_strings(substrings)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
