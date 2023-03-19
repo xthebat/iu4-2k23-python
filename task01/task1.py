98fae10f854df388e6d483e559d69508d84ea0ec
IslomboevBotir <islomboev_botir@mail.ru>
2023-03-19 20:28:02 +0300

    optimization 2

--- task01/task1.py
+++ task01/task1.py
@@ -5,7 +5,7 @@ from os import path
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-f", type=str, required=False, default=r"C:\Users\USER\Desktop\Program\python_prrojects\DZ_MLNI\text.txt")
+    parser.add_argument("-f", type=str, required=True)
     parser.add_argument("-n", type=int, required=False, default=200)
 
     return vars(parser.parse_args())
@@ -29,6 +29,8 @@ def split_the_string(text:str, part_length:int):
 
         if text_part == '':
             return result
+        
+        
 
         if not text_part[-1].isspace() and not text[right].isspace():
             start_words_index = 0
@@ -37,13 +39,37 @@ def split_the_string(text:str, part_length:int):
                     start_words_index = left + i + 1
                     break
             end_words_index = 0
+
             for i in range(start_words_index, len(text)):
                 if text[i].isspace():
                     end_words_index = i
                     break
-            if end_words_index >= right and left != start_words_index:
+            if end_words_index >= right: 
                 right = start_words_index
             text_part = text[left:right]
+
+        start_tag = text.rfind('@', left, right)
+        end_tag = 0
+        if start_tag != -1:
+
+            space_was_found = False
+            for i in range(start_tag, len(text)):
+                if text[i] == ':':
+                    end_tag = i + 1
+                    break
+                if text[i] == ' ' and not space_was_found:
+                    space_was_found = True
+                    continue
+                if text[i].isspace() and text[i] != ' ':
+                    end_tag = i
+                    break
+                if text[i].isspace() and space_was_found:
+                    end_tag = i
+                    break
+            
+            if end_tag > right:
+                right = start_tag
+                text_part = text[left:right]
 
         result.append(text_part)
         left = right
@@ -58,21 +84,21 @@ def main():
     n = arguments['n']
     file_path = arguments['f']
     if n < 1:
-        print("Количество символов в подстроке должно быть больше 0")
-        exit(0)
+        raise ValueError("Количество символов в подстроке должно быть больше 0")
     if not path.exists(file_path):
-        print("Некоректный путь к файлу")
-        exit(0)
+        raise FileNotFoundError("Некоректный путь к файлу")
     with open(file_path, "r", encoding="utf-8") as file:
         text_data = file.read()
 
     text_parts = split_the_string(text_data, n)
     
-    k = 1
-    for part in text_parts:
+    
+    for k, part in enumerate(text_parts, 1):
         print(f"Substring #{k}", part, sep='\n')
-        k += 1
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e.args[0])
