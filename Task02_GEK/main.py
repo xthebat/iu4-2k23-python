2efcfd623b238b05c2cbd07276b2951cfdfc702a
KrawezG <kozlovivan1855@gmail.com>
2023-04-08 15:59:28 +0300

    Добавлен парсинг аргументов на входе программы

--- Task02_GEK/main.py
+++ Task02_GEK/main.py
@@ -1,3 +1,4 @@
+import argparse
 from dataclasses import dataclass
 
 class CHeaderParser:
@@ -41,3 +42,55 @@ class Include(CHeaderParser):
 @dataclass
 class SyntaxError(CHeaderParser):
     message: int
+
+
+def main(args: list[str]) -> int:
+    parser = argparse.ArgumentParser(description="C Header Parser")
+
+    parser.add_argument("-of", "--output-functions", action="store_true", help="output list of all functions")
+    parser.add_argument("-od", "--output-directives", action="store_true", help="output list of all compiler directives")
+    parser.add_argument("-ot", "--output-types", action="store_true", help="output list of all type declarations")
+    parser.add_argument("-f", "--input-file", type=str, help="input file for parsing or saved file")
+    parser.add_argument("-j", "--json", type=str, help="output file in JSON format")
+
+    args = parser.parse_args()
+
+    if args.input_file:
+        # Check the file extension to determine the type of input file
+        if args.input_file.endswith(".c"):
+            # Parse the C header file
+            # ...
+            pass
+        elif args.input_file.endswith(".json"):
+            # Load the saved state from a JSON file
+            # ...
+            pass
+        else:
+            print("Error: unsupported file type")
+            parser.print_usage()
+            return 1
+
+    if args.output_functions:
+        # Output list of all functions
+        # ...
+        pass
+
+    if args.output_directives:
+        # Output list of all compiler directives
+        # ...
+        pass
+
+    if args.output_types:
+        # Output list of all type declarations
+        # ...
+        pass
+
+    if args.json:
+        # Save the output in JSON format to a file
+        # ...
+        pass
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
