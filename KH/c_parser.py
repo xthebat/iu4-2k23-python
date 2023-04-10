de1b3b0264bda8ba572c761aa80e5b01c625d8a8
AlexKreyd <34959278+AlexKreyd@users.noreply.github.com>
2023-04-10 17:20:55 +0300

    add classes of parser, cheaderview, parsed_types

---
+++ KH/c_parser.py
@@ -0,0 +1,32 @@
+from c_header_view import CHeaderView
+
+
+class CHeaderParser:
+
+    def __init__(self, filepath: str, header_view: CHeaderView):
+        self.filepath = filepath
+        self.header_view = header_view
+
+    def __read_file(self):
+        pass
+
+    def parse_file(self, objs_to_parse: dict):
+        pass
+
+    def __parse_functions(self):
+        pass
+
+    def __parse_typedefs(self):
+        pass
+
+    def __parse_defines(self):
+        pass
+
+    def __parse_inline_vars(self):
+        pass
+
+    def __parsed_structs(self):
+        pass
+
+
+
