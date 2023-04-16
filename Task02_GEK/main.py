29447a32e1d48fcdbda440ba872a1ee55121aa59
n.elovskiy <elovskiy00@mail.ru>
2023-04-16 09:21:31 +0300

    Add comments

--- Task02_GEK/main.py
+++ Task02_GEK/main.py
@@ -5,30 +5,69 @@ from dataclasses import dataclass
 
 @dataclass
 class Function:
+    """
+    Экземпляры: найденные функции
+    """
+
     name: str
+    """Имя функции"""
+
     return_type: str
+    """Возвращаемый тип"""
+
     arguments: list
+    """Аргументы функции"""
 
 @dataclass
 class Variable:
+    """
+    Экземпляры: найденные глобальные переменные
+    """
+
     name: str
+    """Имя переменной"""
+
     data_type: str
+    """Тип данных"""
+
     value: str
+    """Значение переменной"""
 
 @dataclass
 class Ifdef:
+    """
+    Экземпляры: найденные ifdef
+    """
+
     expression: str
+    """Выражение условия"""
 
 @dataclass
 class Include:
+    """
+    Экземпляры: найденные include
+    """
+
     filename: str
+    """Имя вкладываемого файла"""
+
     contents: str
+    """Содержание вкладываемого файла"""
 
 @dataclass
 class SyntaxError:
+    """
+    Экземпляры: синтаксические ошибки
+    """
+
     message: int
+    """Содержание ошибки"""
 
 class CHeaderParser:
+    """
+    Интерфейс, описывающий классы, необходимые для парсинга
+    """
+
     def __init__(self):
         self.functions = []
         self.variables = []
@@ -36,16 +75,13 @@ class CHeaderParser:
         self.includes = []
         self.errors = []
         self.line_number = 0
-        
-class CHeaderParser:
-    def __init__(self):
-        self.functions = []
-        self.variables = []
-        self.ifdefs = []
-        self.includes = []
-        self.errors = []
-        self.line_number = 0
-    def set_json(self, input_str):
+        """Номер строки, где найдена структура"""
+
+    def get_json(self, input_str):
+        """
+        Метод, получающий данные из JSON
+        """
+
         data = json.loads(input_str)
         self.functions = [Function(**f) for f in data.get('functions', [])]
         self.variables = [Variable(**v) for v in data.get('variables', [])]
@@ -53,7 +89,12 @@ class CHeaderParser:
         self.includes = [Include(**i) for i in data.get('includes', [])]
         self.errors = [SyntaxError(**e) for e in data.get('errors', [])]
         self.line_number = data.get('line_number', 0)
-    def get_json(self):
+
+    def set_json(self):
+        """
+        Метод, создающий JSON
+        """
+
         data = {
             'functions': [f.__dict__ for f in self.functions],
             'variables': [v.__dict__ for v in self.variables],
@@ -63,10 +104,12 @@ class CHeaderParser:
             'line_number': self.line_number
         }
         return json.dumps(data, indent=4)
+
     def parse(self, str input):
-        ...
+    ...
+
     def print(self):
-        ...
+    ...
 
 
 def main(args: list[str]) -> int:
